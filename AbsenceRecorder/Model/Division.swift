728581c5f031d8816c413a359a58d74e3d472ef5
Morris-T1 <morris.t@etoncollege.org.uk>
2023-01-31 17:29:43 +0000

    adding basic model for divisions and students

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,21 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Fenston Morris on 31/01/2023.
+//
+
+import Foundation
+
+
+class Division {
+    
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String,) {
+        self.code = code
+    }
+    
+    
+}
---
+++ AbsenceRecorder/Model/Student.swift
@@ -0,0 +1,23 @@
+//
+//  Student.swift
+//  AbsenceRecorder
+//
+//  Created by Fenston Morris on 31/01/2023.
+//
+
+import Foundation
+
+class Student {
+    let forename: String
+    let surname: String
+    let birthday: Date
+    
+    init(forename: String, surname: String, birthday: Date) {
+        
+        self.forename = forename
+        self.surname = surname
+        self.birthday = birthday
+    }
+    
+    
+}
